2664bbabd093cb78c3f062f2e6eb1a6835a3608d
calamarte <calamarte@gmail.com>
2017-03-21 17:46:21 +0100

    primera raya

--- src/Bender.java
+++ src/Bender.java
@@ -35,20 +35,48 @@ class Bender {
     // caràcters on cada lletra pot tenir els valors «S», «N», «W» o «E»,
     // segons la posició del robot a cada moment.
     public String run() {
+        StringBuilder resultado = new StringBuilder();
+        char[] direction = new char[]{'S','N','W','E'};
+        int[] position = xposition();
+        int direct = 0;
 
-        List<Integer> position = xposition();
-        System.out.println(position);
+        while (true){
+            int[] aux = avance(position,direction[direct]);
 
-        return null;
+            if (map[aux[0]][aux[1]] == ' '){
+                position = aux;
+                resultado.append(direction[direct]);
+            }
+
+            if (map[aux[0]][aux[1]] == '#'){
+                if (direct == 3){
+                    direct = 0;
+                }else{
+                    direct++;
+                }
+                continue;
+            }
+
+            if (map[aux[0]][aux[1]] == 'T'){}
+
+            if (map[aux[0]][aux[1]] == 'I'){}
+
+            if (map[aux[0]][aux[1]] == '$'){
+                resultado.append(direction[direct]);
+                return resultado.toString();
+            }
+
+        }
+
     }
 
-   private List<Integer> xposition(){
-        List<Integer> position = new ArrayList<>();
+    private int[] xposition(){
+        int[] position = new int[2];
 
         for (int i = 0,y = 1,x = 0; i <(map.length*map.length)- map[0].length ; i++) {
             if (map[y][x] == 'X'){
-                position.add(y);
-                position.add(x);
+                position[0] = y;
+                position[1] = x;
                 return position;
             }
 
@@ -60,6 +88,29 @@ class Bender {
             x++;
         }
 
+        return null;
+    }
+
+    private int[] avance(int[] position,char brujula){
+        switch (brujula){
+
+            case 'S':
+                position[0]++;
+                return position;
+
+            case 'N':
+                position[0]--;
+                return position;
+
+            case 'W':
+                position[1]--;
+                return position;
+
+            case 'E':
+                position[1]++;
+                return position;
+
+        }
         return null;
     }
 }
